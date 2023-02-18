21d5b791efac686754c3ea84510fb32ff6cdd958
Milvirgin <milvirgin999@gmail.com>
2023-02-18 13:12:29 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - entry - point
+ * main - Entry - point
  *
  * Return: Always 0 (success)
  */
@@ -8,12 +8,13 @@
 int main(void)
 {
 	char c;
+
 	c = 'z';
 	while
-		(c >= 'a' {
-		       putchar(c);
-		       c--;
-		 }
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
 	putchar('\n');
-	return (0)
+	return (0);
 }
