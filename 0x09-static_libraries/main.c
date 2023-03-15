57376935a70dd2d3ce62e7b09bb1603e65d527f8
Milvirgin <milvirgin999@gmail.com>
2023-03-15 11:44:44 -0700

    create_static_lib.sh

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puta("\"At the end of the day, my goal was to be the best hacker\"n\t- Kelvin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"n\t- Kelvin Mitnick");
 	return (0);
 }
